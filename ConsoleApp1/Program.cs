f0458cd76429584f0f1313ebe63101aabaecce1e
Your Name <you@example.com>
2018-08-06 16:38:56 +0800

    180806 16.38

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,16 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //NHibernate.Cfg.Configuration configuration = new NHibernate.Cfg.Configuration().Configure();
-            //NHibernate.Tool.hbm2ddl.SchemaExport schemaExport = new NHibernate.Tool.hbm2ddl.SchemaExport(configuration);
-            ////打印Sql脚本,但不执行
-            //schemaExport.Create(true, false);
-            //schemaExport.SetOutputFile(@"c:/hibernate_test.sql");
-            //Console.Read();
-            
-        
-            
+            NHibernate.Cfg.Configuration configuration = new NHibernate.Cfg.Configuration().Configure();
+            NHibernate.Tool.hbm2ddl.SchemaExport schemaExport = new NHibernate.Tool.hbm2ddl.SchemaExport(configuration);
+            //打印Sql脚本,但不执行
+            schemaExport.Create(true, false);
+            schemaExport.SetOutputFile(@"c:/hibernate_test.sql");
+            Console.Read();
         }
-    }
+    
     }
 }
